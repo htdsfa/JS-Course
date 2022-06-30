bbe569a398d0e7754c68a19a97ea9506de200a0b
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-07-01 01:36:15 +0200

    List ToDo

---
+++ Chapter_11/Project_07/Project_07.js
@@ -0,0 +1,117 @@
+let todoInput,
+  errorInfo,
+  addBtn,
+  ulList,
+  newTodos,
+  popup,
+  popupInfo,
+  todoToEdit,
+  popupInput,
+  popupAddBtn,
+  popupCloseBtn;
+
+const main = () => {
+  prepareDOMElements();
+  prepareDOMEvents();
+};
+
+const prepareDOMElements = () => {
+  todoInput = document.querySelector(".todo-input");
+  errorInfo = document.querySelector(".error-info");
+  addBtn = document.querySelector(".btn-add");
+  ulList = document.querySelector(".todoList ul");
+  popup = document.querySelector(".popup");
+  popupInfo = document.querySelector(".popup-info");
+  popupInput = document.querySelector(".popup-input");
+  popupAddBtn = document.querySelector(".accept");
+  popupCloseBtn = document.querySelector(".cancel");
+};
+
+const prepareDOMEvents = () => {
+  addBtn.addEventListener("click", addNewTask);
+  ulList.addEventListener("click", checkClick);
+  popupCloseBtn.addEventListener("click", closePopup);
+  popupAddBtn.addEventListener("click", changeTodoText);
+  todoList.addEventListener("keyup", enterKeyCheck);
+};
+
+const addNewTask = () => {
+  if (todoInput.value !== "") {
+    newTodos = document.createElement("li");
+    newTodos.textContent = todoInput.value;
+    ulList.append(newTodos);
+
+    createToolsArea();
+
+    todoInput.value = "";
+    errorInfo.textContent = "";
+  } else {
+    errorInfo.textContent = "Wpisz tresć zadania";
+  }
+};
+
+const createToolsArea = () => {
+  const toolsPanel = document.createElement("div");
+  toolsPanel.classList.add("tools");
+  newTodos.append(toolsPanel);
+
+  const completeBtn = document.createElement("button");
+  completeBtn.classList.add("complete");
+  completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit");
+  editBtn.textContent = "EDIT";
+
+  const deletBtn = document.createElement("button");
+  deletBtn.classList.add("delete");
+  deletBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+  toolsPanel.append(completeBtn, editBtn, deletBtn);
+};
+
+const checkClick = (e) => {
+  if (e.target.matches(".complete")) {
+    e.target.closest("li").classList.toggle("completed");
+    e.target.classList.toggle("completed");
+  } else if (e.target.matches(".edit")) {
+    editTodo(e);
+  } else if (e.target.matches(".delete")) {
+    dleteTodo(e);
+  }
+};
+
+const editTodo = (e) => {
+  todoToEdit = e.target.closest("li");
+  popupInput.value = todoToEdit.firstChild.textContent;
+  popup.style.display = "flex";
+};
+
+const closePopup = () => {
+  popup.style.display = "none";
+  popupInfo.textContent = "";
+};
+const changeTodoText = () => {
+  if (popupInput.value !== "") {
+    todoToEdit.firstChild.textContent = popupInput.value;
+    popup.style.display = "none";
+    popupInfo.textContent = "";
+  } else {
+    popupInfo.textContent = "Podaj jakąś treść";
+  }
+};
+const deleteTodo = (e) => {
+  e.target.closest("li").remove();
+  const allTodos = ulList.querySelectorAll("li");
+  if (allTodos.length == 0) {
+    errorInfo.textContent = "Bark zadań";
+  }
+};
+
+const enterKeyCheck = (e) => {
+  if (e.key == "Enter") {
+    addNewTask();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", main);
