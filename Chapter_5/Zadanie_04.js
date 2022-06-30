a34e76afee8fe2859affc34f664ffa0892274816
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-07-01 00:42:44 +0200

    case task

---
+++ Chapter_5/Zadanie_04.js
@@ -0,0 +1,16 @@
+const promo = '20%'
+
+switch (promo) {
+    case "10%":
+        console.log('Dzis mamy 10% zniźki!')
+        break
+    case "20%":
+        console.log('Dzis mamy 20% zniźki!')
+        break
+    case "30%":
+        console.log('Dzis mamy 30% zniźki!')
+        break
+    default:
+        console.log('Dzis mamy niestandardowa prmocje')
+        break
+}
