c26fc3fde04a8c3ed92b01effd3e12e33ff050d9
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-07-01 00:44:58 +0200

    if x>=100

---
+++ Chapter_5/Zadanie_06.js
@@ -0,0 +1,8 @@
+const x = 50
+if (x >= 100) {
+    console.log("x>=100".toUpperCase())
+} else if (x < 100 && x > 30) {
+    console.log("x jest średniakiem".toUpperCase())
+} else {
+    console.log("x jest mały".toUpperCase())
+}
